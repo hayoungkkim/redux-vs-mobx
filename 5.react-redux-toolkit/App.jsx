55530266d8242932ed6dd39f1f4ea65a6cea43b1
202113 <202113@ssg.com>
2021-11-17 00:57:26 +0900

    3-7. createSelector(reselect)

--- 5.react-redux-toolkit/App.jsx
+++ 5.react-redux-toolkit/App.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { createSelector } from "@reduxjs/toolkit"; // reselect
 
 const { logIn } = require("./actions/user");
-const { addPost } = require("./actions/post");
 const userSlicer = require("./reducers/userSlice");
+
+const priceSelector = (state) => state.user.prices;
+const sumPriceSelector = createSelector(priceSelector, (prices) => prices.reduce((a, c) => a + c, 0));
 
 const App = () => {
 	const user = useSelector((state) => state.user);
-	const { list } = useSelector((state) => state.post);
+	const totalPrice = useSelector(sumPriceSelector);
 	const dispatch = useDispatch();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
@@ -48,10 +51,18 @@ const App = () => {
 		[dispatch, email, password]
 	);
 
+	// const totalPrice = useMemo(() => {
+	// 	console.log("memo");
+	// 	return prices.reduce((a, c) => a + c, 0); // 100만번 연산이라 가정
+	// }, [prices]);
+
 	return (
 		<div>
 			{user.isLoggingIn ? <div>로그인 중</div> : user.data ? <div>{user.data.nickname}</div> : "로그인 해주세요."}
 			{!user.data ? <button onClick={onClick}>로그인</button> : <button onClick={onLogout}>로그아웃</button>}
+			<div>
+				<b>{totalPrice}원</b>
+			</div>
 			<form action={onSubmit}>
 				<input type="email" value={email} onChange={onChangeEmail} />
 				<input type="password" value={password} onChange={onChangePassword} />
