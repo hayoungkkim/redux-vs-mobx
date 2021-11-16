4be1f44b7aebb4be11b38c04978eea81e7f8df7e
202113 <202113@ssg.com>
2021-11-16 14:42:26 +0900

    2-4. redux-thunk

--- 2.redux/actions/user.js
+++ 2.redux/actions/user.js
@@ -1,8 +1,41 @@
+// async action creator
 const logIn = (data) => {
-	// action
+	return (dispatch, getState) => {
+		dispatch(logInRequest(data));
+		try {
+			setTimeout(() => {
+				dispatch(
+					logInSuccess({
+						userId: 1,
+						nickname: "hayoung",
+					})
+				);
+			}, 2000);
+		} catch (e) {
+			dispatch(logInFailure(e));
+		}
+	};
+};
+
+// sync action creator
+const logInRequest = (data) => {
 	return {
-		type: "LOG_IN",
+		type: "LOG_IN_REQUEST",
 		data,
+	};
+};
+
+const logInSuccess = (data) => {
+	return {
+		type: "LOG_IN_SUCCESS",
+		data,
+	};
+};
+
+const logInFailure = (error) => {
+	return {
+		type: "LOG_IN_FAILURE",
+		error,
 	};
 };
 
--- 2.redux/index2.js
+++ 2.redux/index2.js
@@ -11,26 +11,23 @@ const initialState = {
 	posts: [],
 };
 
-const firstMiddleware = (store) => (dispatch) => (action) => {
-	// dispatch 전 : 기능 추가
-	console.log("액션 로깅\n", action);
-	dispatch(action);
-	// dispatch 후 : 기능 추가
-	console.log("액션 끝");
+const firstMiddleware = (store) => (next) => (action) => {
+	console.log("로깅", action);
+	next(action);
 };
-// function firstMiddleware(store) {
-// 	return function (next) {
-// 		return function (action) {
-// 		}
-// 	}
-// }
-const enhancer = applyMiddleware(firstMiddleware);
+
+const thunkMiddleware = (store) => (next) => (action) => {
+	// 비동기
+	if (typeof action === "function") {
+		return action(store.dispatch, store.getState);
+	}
+	// 동기
+	return next(action);
+};
+
+const enhancer = applyMiddleware(firstMiddleware, thunkMiddleware);
 
 const store = createStore(reducer, initialState, enhancer);
-// react-redux 안에 들어있어요.
-store.subscribe(() => {
-	console.log("changed"); // 화면 바꿔주는 코드 여기서
-});
 
 console.log("1st", store.getState());
 
@@ -45,23 +42,23 @@ store.dispatch(
 );
 console.log("2nd", store.getState());
 
-store.dispatch(
-	addPost({
-		userId: 1,
-		id: 1,
-		content: "안녕하세요. 리덕스",
-	})
-);
-console.log("3rd", store.getState());
+// store.dispatch(
+// 	addPost({
+// 		userId: 1,
+// 		id: 1,
+// 		content: "안녕하세요. 리덕스",
+// 	})
+// );
+// console.log("3rd", store.getState());
 
-store.dispatch(
-	addPost({
-		userId: 1,
-		id: 2,
-		content: "두번째 리덕스",
-	})
-);
-console.log("4th", store.getState());
+// store.dispatch(
+// 	addPost({
+// 		userId: 1,
+// 		id: 2,
+// 		content: "두번째 리덕스",
+// 	})
+// );
+// console.log("4th", store.getState());
 
-store.dispatch(logOut());
-console.log("5th", store.getState());
+// store.dispatch(logOut());
+// console.log("5th", store.getState());
